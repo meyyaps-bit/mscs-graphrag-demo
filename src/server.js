b111e43a6d4ed940a8748b197389f96bedc79901
meyyaps-bit <meyyaps@gmail.com>
2026-03-27 18:01:34 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -20,5 +20,9 @@ app.get("/", (req, res) => {
   res.send("GraphRAG demo is running.");
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on ${port}`));
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
